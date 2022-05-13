e2bdda79231c647a61b8e2566f57e920eda9086c
Andrew Walters <andrewjpwalters@gmail.com>
2022-05-12 23:29:22 -0500

    add form event listener

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+//DOM Selectors
 const form = document.querySelector('#searchForm')
 const watchList = document.querySelector('#watchList')
+
+//Event Listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(form.elements.query.value)
+    console.log(e)
+})
