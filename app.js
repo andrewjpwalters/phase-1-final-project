0125ba7a4439252edf95977fe7d80422efc9c1ce
Andrew Walters <andrewjpwalters@gmail.com>
2022-05-15 22:08:42 -0500

    add render function for search results

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //DOM Selectors
 const form = document.querySelector('#searchForm')
 const watchList = document.querySelector('#watchList')
+const searchResults = document.querySelector('#searchResults')
 
 //Event Listeners
 form.addEventListener('submit', (e) => {
@@ -8,5 +9,31 @@ form.addEventListener('submit', (e) => {
     const searchInput = form.elements.query.value
     fetch(`https://api.tvmaze.com/search/shows?q=${searchInput}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            data.forEach(show => renderShows(show))
+        })
 })
+
+//Handler functions
+function renderShows(showResult) {
+    let card = document.createElement('li')
+    if (showResult.show.image) {
+        if (showResult.show.summary === null) {
+            card.innerHTML = `
+    <img src="${showResult.show.image.medium}">
+    <h4>${showResult.show.name}</h4>
+    <p> No Summary Available </p>
+    <button id="addWatchList"> Add to Watch List </button>
+    `
+        } else {
+            card.innerHTML = `
+    <img src="${showResult.show.image.medium}">
+    <h4>${showResult.show.name}</h4>
+    ${showResult.show.summary}
+    <button id="addWatchList"> Add to Watch List </button>
+    `
+        }
+    }
+    searchResults.appendChild(card)
+}
