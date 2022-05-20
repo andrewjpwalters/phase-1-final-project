c9b1a2d60f177ba3eec27b38fa811ae825de30a1
Andrew Walters <andrewjpwalters@gmail.com>
2022-05-19 20:52:56 -0500

    add initial boostrap styling and extra conditional to renderShows function to fix rendering error

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ form.addEventListener('submit', (e) => {
     fetch(`https://api.tvmaze.com/search/shows?q=${searchInput}`)
         .then(res => res.json())
         .then(data => {
+            console.log(data)
             data.forEach(show => renderShows(show))
         })
-        .catch((e) => {
+        .catch(e => {
             console.log('ERROR:', e)
         })
     form.elements.query.value = '';
@@ -34,24 +35,47 @@ openWatchList.addEventListener('click', () => {
 //Handler function
 function renderShows(showResult) {
     //Renders show card from search and appends to the "Search Results" section
-    let card = document.createElement('li')
+    let card = document.createElement('div')
     let isInWatchList = false
-    if (showResult.show.image) {
+    card.setAttribute("class", "card")
+    if (showResult.show.image !== null) {
         if (showResult.show.summary === null) {
             card.innerHTML = `
-    <img src="${showResult.show.image.medium}">
-    <h4>${showResult.show.name}</h4>
-    <p> No Summary Available </p>
-    <button id="addWatchList"> Add to Watch List </button>
+    <img class="card-img-top" src="${showResult.show.image.medium}">
+    <div class="card-body">
+    <h5 class="card-title">${showResult.show.name}</h5>
+    <p class="card-text"> No Summary Available </p>
+    <button class="btn btn-primary" id="addWatchList"> Add to Watch List </button>
+    </div>
     `
         } else {
             card.innerHTML = `
-    <img src="${showResult.show.image.medium}">
-    <h4>${showResult.show.name}</h4>
-    ${showResult.show.summary}
-    <button id="addWatchList"> Add to Watch List </button>
+    <img class="card-img-top" src="${showResult.show.image.medium}">
+    <div class="card-body">
+    <h5 class="card-title">${showResult.show.name}</h5>
+    <p class="card-text"> ${showResult.show.summary} </p>
+    <button class="btn btn-primary" id="addWatchList"> Add to Watch List </button>
+    </div>
     `
         }
+    } else if (showResult.show.summary !== null) {
+        card.innerHTML = `
+    <img class="card-img-top" src="imgs/no-img-portrait-text.png">
+    <div class="card-body">
+    <h5 class="card-title">${showResult.show.name}</h5>
+    <p class="card-text"> ${showResult.show.summary} </p>
+    <button class="btn btn-primary" id="addWatchList"> Add to Watch List </button>
+    </div>
+    `
+    } else {
+        card.innerHTML = `
+    <img class="card-img-top" src="imgs/no-img-portrait-text.png">
+    <div class="card-body">
+    <h5 class="card-title">${showResult.show.name}</h5>
+    <p class="card-text"> No Summary Available </p>
+    <button class="btn btn-primary" id="addWatchList"> Add to Watch List </button>
+    </div>
+    `
     }
     //Clones show to "Watch List", removes "Add to Watch List" button
     //and adds "Remove" button
@@ -61,6 +85,7 @@ function renderShows(showResult) {
             let removeButton = document.createElement('button')
             newCard.querySelector('#addWatchList').remove()
             removeButton.setAttribute("id", "remove")
+            removeButton.setAttribute("class", "btn btn-primary")
             removeButton.innerText = 'Remove'
             newCard.appendChild(removeButton)
             newCard.querySelector('#remove').addEventListener('click', () => {
